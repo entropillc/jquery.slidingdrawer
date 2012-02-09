fb28072d588eb657e48231114582380d856ddf07
Justin D'Arcangelo <justindarc@gmail.com>
2012-02-09 01:26:48 -0500

    Fixed graphical glitches with 3D transforms when used in conjunction with Pushpop/ScrollKit.

--- jquery.slidingdrawer.js
+++ jquery.slidingdrawer.js
@@ -5,6 +5,20 @@ $(function() {
   var delta = 0;
   var rotation = -90;
   
+  // TODO: REMOVE THIS!! - TEMP FIX TO AUTO-CLOSE DRAWER ON MODAL LAUNCH.
+  drawer.data('closeDrawer', function() {
+    rotation = -90;
+    
+    var transform = 'perspective(2000) translate3d(0, 0, 30px) rotate3d(0, 1, 0, ' + rotation + 'deg)';
+    
+    drawer.css({
+      '-webkit-transition': 'all 0.5s',
+      'transition': 'all 0.5s',
+      '-webkit-transform': transform,
+      'transform': transform
+    });
+  });
+  
   drawer.bind('mousedown touchstart', function(evt) {
     var e = evt.originalEvent;
     
@@ -12,12 +26,15 @@ $(function() {
     lastMouseX = evt.pageX || ((e.touches && e.touches.length) ? e.touches[0].pageX : 0);
     delta = 0;
     
+    var transform = 'perspective(2000) translate3d(0, 0, 30px) rotate3d(0, 1, 0, ' + rotation + 'deg)';
+    
     drawer.css({
       '-webkit-transition': 'none',
       'transition': 'none',
-      '-webkit-transform': 'perspective(2000) rotate3d(0, 1, 0, ' + rotation + 'deg)',
-      'transform': 'perspective(2000) rotate3d(0, 1, 0, ' + rotation + 'deg)'
+      '-webkit-transform': transform,
+      'transform': transform
     });
+    
     evt.preventDefault();
   });
   
@@ -38,9 +55,11 @@ $(function() {
     
     lastMouseX = mouseX;
     
+    var transform = 'perspective(2000) translate3d(0, 0, 30px) rotate3d(0, 1, 0, ' + rotation + 'deg)';
+    
     drawer.css({
-      '-webkit-transform': 'perspective(2000) rotate3d(0, 1, 0, ' + rotation + 'deg)',
-      'transform': 'perspective(2000) rotate3d(0, 1, 0, ' + rotation + 'deg)'
+      '-webkit-transform': transform,
+      'transform': transform
     });
     
     evt.preventDefault();
@@ -63,11 +82,13 @@ $(function() {
     lastMouseX = 0;
     delta = 0;
     
+    var transform = 'perspective(2000) translate3d(0, 0, 30px) rotate3d(0, 1, 0, ' + rotation + 'deg)';
+    
     drawer.css({
       '-webkit-transition': 'all 0.3s',
       'transition': 'all 0.3s',
-      '-webkit-transform': 'perspective(2000) rotate3d(0, 1, 0, ' + rotation + 'deg)',
-      'transform': 'perspective(2000) rotate3d(0, 1, 0, ' + rotation + 'deg)'
+      '-webkit-transform': transform,
+      'transform': transform
     });
     
     evt.preventDefault();
